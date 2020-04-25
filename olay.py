6c3bfb220ef6e6871b8853d4126f1a3238eac33e
crackDAbottle <hypersamet@gmail.com>
2020-04-25 18:04:12 +0300

    new py

---
+++ olay.py
@@ -0,0 +1,8 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+
+>>> a = [x for x in [6,7,8] if x is not 6]
+>>> a
+[7, 8]
+>>> 
